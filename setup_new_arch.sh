e1ec5432bea73a697ef5254f9cb4e462d067e5e7
300degree <kitibodee05p@gmail.com>
2024-03-04 20:25:27 +0700

    fix error, and test something

--- setup_new_arch.sh
+++ setup_new_arch.sh
@@ -3,13 +3,13 @@
 install_tools() {
     echo -e "\nInstalling tools..."
     sudo pacman -Syu \
-        neovim tmux fish exa yay git neofetch \
+        neovim tmux fish exa git neofetch \
         nodejs npm pnpm \
         ttf-firacode-nerd
 
     echo "[*] Install oh-my-fish & theme"
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-    omf install neolambda
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
+    cp -r ./theme/neolambda ~/.local/share/omf/themes
 
     echo "[*] Install tmp"
     git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
@@ -17,20 +17,21 @@ install_tools() {
 
 copy_config() {
     echo -e "\nCopying configs..."
-    echo "[*] Copying fishconf"
-    cp ./.config/fish ~/.config/fish
-    source ~/.config/fish/conf.fish
-
     echo "[*] Copying neofetch"
-    cp ./.config/neofetch/ ~/.config/neofetch/
+    cp -r .config/neofetch/ ~/.config/
 
     echo "[*] Copying neovim"
-    cp ./.config/nvim ~/.config/nvim
+    cp -r ./.config/nvim ~/.config/nvim
 
     echo "[*] Copying tmuxconf"
-    cp ./.config/tmux/tmux.conf ~/.config/tmux
-    source ~/.config/tmux/tmux.conf
+    cp -r ./.config/tmux/tmux.conf ~/.config/tmux
+
+    echo "[*] Copying fishconf"
+    cp -r .config/fish/ ~/.config/fish
 }
 
 install_tools
 copy_config
+
+echo "[*] Changing default shell"
+chsh -s "$(which fish)" | fish
