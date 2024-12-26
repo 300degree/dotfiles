a3fff1a05aae86a1943215848f39c86bb679ffd4
300degree <kitibodee05p@gmail.com>
2024-12-26 20:52:58 +0700

    deleted:    .config/fish/config.fish 	deleted:    .config/neofetch/config.conf 	deleted:    .config/neofetch/fumo.txt 	renamed:    .tmux.conf -> .config/tmux/tmux.conf 	new file:   .config/vim/vimrc 	renamed:    .zshrc -> .config/zsh/zshrc 	deleted:    .vimrc 	deleted:    .zshrc_kit_profile 	deleted:    image/300degree.png 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,99 +1,33 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
-ZSHRC_PROFILE="https://raw.githubusercontent.com/300degree/dotfiles/refs/heads/main/.zshrc_kit_profile"
-TMUXCONF_URL="https://raw.githubusercontent.com/300degree/dotfiles/refs/heads/main/.tmux.conf"
-VIMCONF_URL="https://raw.githubusercontent.com/300degree/dotfiles/refs/heads/main/.vimrc"
+URL="https://raw.githubusercontent.com/300degree/dotfiles/main"
 
-detect_vim_type() {
-    if command -v nvim >/dev/null 2>&1; then
-        echo "nvim"
-    else
-        echo "vim"
-    fi
+success()
+{
+  printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-install_node() {
-    if ! command -v node &>/dev/null; then
-		echo "Installing nodejs"
-		mkdir -p $HOME/.nodejs
-		curl -sL install-node.vercel.app/lts | bash -s -- --prefix=$HOME/.nodejs
-    fi
-}
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
+    --unattended --keep-zshrc
 
-install_fzf() {
-    echo "Installing fzf"
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-}
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-install_omz() {
-    echo "Installing oh_my_zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-}
+mkdir -p ~/.config/vim
+curl -sfL "$URL/.config/vim/vimrc" -o ~/.config/vimrc
+ln -sf ~/.config/vim/vimrc ~/.vimrc
+success "Install vimrc"
 
-install_vim_plugin() {
-    local vim_type=$(detect_vim_type)
-    local plug_path
+mkdir -p ~/.config/tmux
+curl -sfL "$URL/.config/tmux/tmux.conf" -o ~/.config/tmux.conf
+ln -sf ~/.config/tmux/tmux.conf ~/.tmux.conf
+success "Install tmuxconf"
 
-    if [[ "$vim_type" == "nvim" ]]; then
-        plug_path="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
-    else
-        plug_path="$HOME/.vim/autoload/plug.vim"
-    fi
+mkdir -p ~/.config/zsh
+curl -sfL "$URL/.config/zsh/zshrc" -o ~/.config/zshrc
+ln -sf ~/.config/zsh/zshrc ~/.zshrc
+success "Install zshrc"
 
-    echo "Installing vim-plugin"
-    curl -fLo "$plug_path" --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-}
+# mkdir -p ~/.config/git
 
-create_tmuxconf() {
-    echo "Creating tmuxconf"
-    curl -sfL \
-        "$TMUXCONF_URL" -o \
-        "$HOME/Documents/setup-dotfile/tmux.conf"
-}
-
-create_vimconf() {
-    local vim_type=$(detect_vim_type)
-    local config_path
-    local config_file
-
-    if [[ "$vim_type" == "nvim" ]]; then
-        config_file="init.vim"
-        config_path="$HOME/.config/nvim"
-        mkdir -p "$config_path"
-    else
-        config_file=".vimrc"
-        config_path="$HOME"
-    fi
-
-    echo "Creating vimrc or init.vim"
-    curl -sfL \
-        "$VIMCONF_URL" -o \
-        "$config_path/$config_file"
-}
-
-added_profile() {
-    curl -sfL \
-        "$ZSHRC_PROFILE" -o \
-        "$HOME/.zshrc_kit_profile"
-
-    if grep -q "^ZSH_THEME=" $HOME/.zshrc; then
-        sed -i 's/^ZSH_THEME=.*/ZSH_THEME="half-life"/' $HOME/.zshrc
-    else
-        echo 'ZSH_THEME="half-life"' >> $HOME/.zshrc
-    fi
-    echo "source $HOME/.zshrc_kit_profile"
-    echo "ZSH_PROFILE added successfully."
-}
-
-detect_vim_type
-install_node
-install_fzf
-install_omz
-install_vim_plugin
-create_tmuxconf
-create_vimconf
-added_profile
-
-echo "Done! : Dont forget to run :PlugInstall in vim or neovim"
+printf "Done! : Dont forget to run :PlugInstall in VIM and change default SHELL"
