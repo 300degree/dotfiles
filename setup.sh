610521cd81af9e29c8c5fa2eda883c9153cb946a
300degree <kitibodee05p@gmail.com>
2025-01-17 04:09:30 +0700

    updated: script install gitconfig

--- setup.sh
+++ setup.sh
@@ -7,6 +7,21 @@ success()
   printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n"
 }
 
+failure()
+{
+  printf "\r\033[2K [ \033[00;31mFAIL\033[0m ] $1\n"
+}
+
+download()
+{
+    if curl -sfL "$1" -o "$2"; then
+        success "$3"
+    else
+        failure "$3"
+        exit 1
+    fi
+}
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
     --unattended --keep-zshrc
 
@@ -14,20 +29,19 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/vim
-curl -sfL "$URL/.config/vim/vimrc" -o ~/.config/vim/vimrc
+download "$URL/.config/vim/vimrc" ~/.config/vim/vimrc "Install vimrc"
 ln -sf ~/.config/vim/vimrc ~/.vimrc
-success "Install vimrc"
 
 mkdir -p ~/.config/tmux
-curl -sfL "$URL/.config/tmux/tmux.conf" -o ~/.config/tmux/tmux.conf
+download "$URL/.config/tmux/tmux.conf" ~/.config/tmux/tmux.conf "Install tmux.conf"
 ln -sf ~/.config/tmux/tmux.conf ~/.tmux.conf
-success "Install tmuxconf"
 
 mkdir -p ~/.config/zsh
-curl -sfL "$URL/.config/zsh/zshrc" -o ~/.config/zsh/zshrc
+download "$URL/.config/zsh/zshrc" ~/.config/zsh/zshrc "Install zshrc"
 ln -sf ~/.config/zsh/zshrc ~/.zshrc
-success "Install zshrc"
 
-# mkdir -p ~/.config/git
+mkdir -p ~/.config/git
+download "$URL/.config/git/config" ~/.config/git/config "Install gitconfig"
+download "$URL/.config/git/ignore" ~/.config/git/ignore "Install gitignore"
 
 printf "Done! : Dont forget to run :PlugInstall in VIM and change default SHELL\n"
