059fa0b1750efd1de5e10f1e1c23b316fe982bd9
300degree <kitibodee05p@gmail.com>
2025-02-10 21:48:27 +0700

    move to basic and add clang-format, fzf script

--- setup.sh
+++ setup.sh
@@ -2,46 +2,32 @@
 
 URL="https://raw.githubusercontent.com/300degree/dotfiles/main"
 
-success()
-{
-  printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-failure()
-{
-  printf "\r\033[2K [ \033[00;31mFAIL\033[0m ] $1\n"
-}
-
-download()
-{
-    if curl -sfL "$1" -o "$2"; then
-        success "$3"
-    else
-        failure "$3"
-        exit 1
-    fi
-}
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
     --unattended --keep-zshrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+[ ! -f ~/.vim/autoload/plug.vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/vim
-download "$URL/.config/vim/vimrc" ~/.config/vim/vimrc "Install vimrc"
-ln -sf ~/.config/vim/vimrc ~/.vimrc
+# printf "Install vimrc\n"
+[ ! -f ~/.vimrc ] && curl -sfL "$URL/.config/vim/vimrc" -o ~/.vimrc
 
-mkdir -p ~/.config/tmux
-download "$URL/.config/tmux/tmux.conf" ~/.config/tmux/tmux.conf "Install tmux.conf"
-ln -sf ~/.config/tmux/tmux.conf ~/.tmux.conf
+# printf "Install .tmux.conf\n"
+[ ! -f ~/.tmux.conf ] && curl -sfL "$URL/.config/tmux/tmux.conf" -o ~/.tmux.conf
 
-mkdir -p ~/.config/zsh
-download "$URL/.config/zsh/zshrc" ~/.config/zsh/zshrc "Install zshrc"
-ln -sf ~/.config/zsh/zshrc ~/.zshrc
+# printf "Install .zshrc\n"
+[ ! -f ~/.zshrc ] &&
+curl -sfL "$URL/.config/zsh/zshrc" -o ~/.zshrc
 
-mkdir -p ~/.config/git
-download "$URL/.config/git/config" ~/.config/git/config "Install gitconfig"
-download "$URL/.config/git/ignore" ~/.config/git/ignore "Install gitignore"
+# printf "Install .gitconfig\n"
+[ ! -f ~/.gitconfig ] && curl -sfL "$URL/.config/git/config" -o ~/.gitconfig
+[ ! -f ~/.gitignore ] && curl -sfL "$URL/.config/git/ignore" -o ~/.gitignore
+
+# printf "Install .clang-format"
+curl -sfL "$URL/.clang-format" -o ~/.clang-format
+
+# dotfile log and etc [TODO]
+mkdir -p ~/.local/bin
+[ ! -f ~/.local/bin/df-log ] && curl -sfL "$URL/.local/bin/df-log" -o ~/.local/bin/df-log
+# [ ! -f ~/.local/bin/df- ] && curl -sfL "$URL/.local/bin/df-" -o ~/.local/bin/df-
 
 printf "Done! : Dont forget to run :PlugInstall in VIM and change default SHELL\n"
