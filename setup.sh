69d28b10233724f3df3c74d295ff45cde267cef9
300degree <kitibodee05p@gmail.com>
2025-04-06 17:07:34 +0700

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,32 @@
 #! /usr/bin/env bash
+set -e
 
 URL="https://raw.githubusercontent.com/300degree/dotfiles/main"
+BIN_PREFIX=~/.local/bin
 
-[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
-    --unattended --keep-zshrc
+mkdir -p ~/.vim/autoload
+mkdir -p $BIN_PREFIX
 
-[ ! -f ~/.vim/autoload/plug.vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(wget -O-                                     \
+  https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  \
+  "" --unattended
 
-wget -O .oh-my-zsh/themes/gruvbox.zsh-theme \
-    https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/refs/heads/master/gruvbox.zsh-theme
+[ ! -f ~/.vim/autoload/plug.vim ] && wget -O ~/.vim/autoload/plug.vim          \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# printf "Install vimrc\n"
-[ ! -f ~/.vimrc ] && curl -sfL "$URL/.config/vim/vimrc" -o ~/.vimrc
+[ ! -f ~/.vimrc ] && wget -O ~/.vimrc "$URL/.vimrc" 
+[ ! -f ~/.gitconfig ] && wget -O ~/.gitconfig "$URL/.gitconfig"
+[ ! -f ~/.tmux.conf ] && wget -O ~/.tmux.conf "$URL/.tmux.conf"
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bck && wget -O ~/.zshrc "$URL/.zshrc" 
 
-# printf "Install .tmux.conf\n"
-[ ! -f ~/.tmux.conf ] && curl -sfL "$URL/.config/tmux/tmux.conf" -o ~/.tmux.conf
+fzf_script=("git-commit-explore" "git-branch-explore" "vim-explore")
+for out in "${fzf_script[@]}"
+do
+  wget -O $BIN_PREFIX/$out "$URL/bin/$out"
+  if [ -f $BIN_PREFIX/$out ]; then
+    chmod +x ~/.local/bin/$out
+  fi
+done
 
-# printf "Install .zshrc\n"
-[ ! -f ~/.zshrc ] && curl -sfL "$URL/.config/zsh/zshrc" -o ~/.zshrc
-
-# printf "Install .gitconfig\n"
-[ ! -f ~/.gitconfig ] && curl -sfL "$URL/.config/git/config" -o ~/.gitconfig
-[ ! -f ~/.gitignore ] && curl -sfL "$URL/.config/git/ignore" -o ~/.gitignore
-
-# dotfile log and etc [TODO]
-mkdir -p ~/.local/bin
-[ ! -f ~/.local/bin/df-log ] && curl -sfL "$URL/bin/df-log" -o ~/.local/bin/df-log
-[ ! -f ~/.local/bin/df-co ] && curl -sfL "$URL/bin/df-co" -o ~/.local/bin/df-co
-chmod +x ~/.local/bin/*
-
-printf "Done! : Dont forget to run :PlugInstall in VIM and change default SHELL\n"
+echo "Done!"
+echo "Dont forget to run :PlugInstall in VIM and change default SHELL"
